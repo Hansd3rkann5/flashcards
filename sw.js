d28fb1e8db07e6e25c79a772889c02e57bd0929d
simonbader <simon-bader@gmx.net>
2026-08-10 21:49:56 +0200

    [v2026.08.10.1] Dynamic MCQ Face content

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE = 'flashcards-static-v5';
-const RUNTIME_CACHE = 'flashcards-runtime-v5';
-const API_CACHE = 'flashcards-api-v5';
+const STATIC_CACHE = 'flashcards-static-v6';
+const RUNTIME_CACHE = 'flashcards-runtime-v6';
+const API_CACHE = 'flashcards-api-v6';
 
 const APP_SHELL = [
   './',
